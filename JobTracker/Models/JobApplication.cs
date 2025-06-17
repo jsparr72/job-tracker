6459a155a195b1e7dc5c3bb039bbd2a8b07268ba
jsparr72 <sparrjack72@gmail.com>
2025-06-17 10:28:12 -0400

    Added properties and constructor to job application class

--- JobTracker/Models/JobApplication.cs
+++ JobTracker/Models/JobApplication.cs
@@ -2,5 +2,25 @@
 
 public class JobApplication
 {
+    public int Id { get; set; }
+    public string UserId { get; set; } // For ASP.NET Identity
+    public string Company { get; set; }
+    public string JobTitle { get; set; }
+    public DateTime ApplicationDate { get; set; }
+    public string Status { get; set; } // Applied, Interview, Rejected, etc.
+    public string Notes { get; set; }
 
+    public JobApplication(int Id, string UserId, string Company,
+            string JobTitle, DateTime ApplicationDate, string Status,
+            string Notes = null)
+    {
+
+        this.Id = Id;
+        this.UserId = UserId;
+        this.Company = Company;
+        this.JobTitle = JobTitle;
+        this.ApplicationDate = ApplicationDate;
+        this.Status = Status;
+        this.Notes = Notes;
+    }
 }
