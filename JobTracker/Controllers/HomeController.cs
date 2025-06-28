05f639c5e16f22b5d1571f7148f4499c119188f9
jsparr72 <sparrjack72@gmail.com>
2025-06-28 14:27:41 -0400

    Added editing and deleting functions

--- JobTracker/Controllers/HomeController.cs
+++ JobTracker/Controllers/HomeController.cs
@@ -32,30 +32,38 @@ public class HomeController : Controller
         return View(ApplicationsTable);
     }
 
-    public IActionResult CreateEditApplications()
+    public IActionResult CreateApplication()
     {
         return View();
     }
 
-    public IActionResult NewApplication(JobApplication model)
+    public IActionResult EditApplication(int ID)
+    {
+        var applicationInDatabase = _context.Job_Applications.SingleOrDefault(application => application.ID == ID);
+        return View(applicationInDatabase);
+    }
+
+    public IActionResult DeleteApplication(int ID)
+    {
+        var applicationInDatabase = _context.Job_Applications.SingleOrDefault(application => application.ID == ID);
+        _context.Job_Applications.Remove(applicationInDatabase);
+        _context.SaveChanges();
+        return RedirectToAction("Applications");
+    }
+
+    public IActionResult NewApplication(JobApplication model) // When a new application is submitted through form
     {
         _context.Job_Applications.Add(model);
         _context.SaveChanges();
         return RedirectToAction("Applications");
     }
 
-    // public IActionResult Create()
-    // {
-    //     var model = new JobApplication
-    //     {
-    //         Statuses = new List<SelectListItem>
-    //         {
-    //             new SelectListItem { Value = "1", Text = "Not Applied" },
-    //             new SelectListItem {Value = "2", Text = "Applied"}
-    //         }
-    //     };
-    //     return View(model);
-    // }
+    public IActionResult ModifyApplication(JobApplication model) // When an application is being edited
+    {
+        _context.Job_Applications.Update(model);
+        _context.SaveChanges();
+        return RedirectToAction("Applications");
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
